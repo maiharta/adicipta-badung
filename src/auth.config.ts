30132d42fec625d3aeca865d63384a337d21ac0e
gusweda219 <57088126+gusweda219@users.noreply.github.com>
2024-09-18 22:32:07 +0800

    chore: change session maxAge

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -4,7 +4,7 @@ import type { NextAuthConfig } from "next-auth";
 export const authConfig = {
   session: {
     strategy: "jwt",
-    maxAge: 3 * 60,
+    maxAge: 1 * 60,
   },
   providers: [],
   pages: {
